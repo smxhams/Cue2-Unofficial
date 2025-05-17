dd13d3d9790dd3fa5cd3e67ec89e4175eb84bcab
smxhams <smxham@gmail.com>
2025-05-17 21:48:52 +1200

    Started work on settings window. Added UI scaling as test for system settings conf.

---
+++ src/Base/Settings/SettingsGeneral.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+namespace Cue2.Base.Settings;
+
+public partial class SettingsGeneral : ScrollContainer
+{
+    public override void _Ready()
+    {
+        GD.Print("Settingsd General Init");
+    }
+}
